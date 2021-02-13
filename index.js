ef652724e4d446883b37ce555086f646d197c508
Wesley <wspencer3295@yahoo.com>
2021-02-13 13:25:56 -0700

    commit tree eae65d10e19af7b4bd188b70ca7f41981c7cd8dc

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+   const hour = parseInt(time);
+
+  if (hour < 12){
+     return "Good Morning";
+  }
+
+  else if (hour > 17) {
+     return "Good Evening";
+  } 
+
+  else {
+     return "Good Afternoon"
+  }
+
+  }
 /* Write your implementation of displayMessage() */
+ function displayMessage(msg){
+   document.getElementById('greeting').innerText = msg;
+ }
